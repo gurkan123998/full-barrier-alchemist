d37ae90ccbc21c5caf5eb22cdf389bc1a31e92f6
gurkan <e222713@metu.edu.tr>
2020-02-08 00:36:59 +0300

    full barrier alchemist

---
+++ full_barrier_alc.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+int Test;
+int Numb;
+float Height;
+float Life;
+float Duck_Y1;
+float Jump_Y2;
+int N;
+int counter=0;
+
+int t;
+int X;
+scanf("%d",&Test);
+int result[Test];
+
+for (t= 0;t<Test;t++){ 
+counter=0;
+scanf("%d %f %f %f %f",&Numb,&Height,&Duck_Y1,&Jump_Y2,&Life);
+int type[Numb];
+for (N= 0;N<Numb;N++){
+scanf("%d %d",&type[N],&X);
+	counter++;
+	result[t]=counter;
+	if(type[N]==1){	
+		
+		if(Height-Duck_Y1>X){
+			Life--;
+			if(Life==0){
+				result[t]=counter-1;
+				break;
+	
+			}
+	
+		}
+		
+	}
+	else if(type[N]==2){
+		
+		if(Jump_Y2<X){
+			Life--;
+			if(Life==0){
+				result[t]=counter-1;
+				break;
+			}
+		}
+	}
+
+}
+
+}
+for(int i=0;i<Test;i++){
+	printf("%d\n",result[i]);
+}	
+return 0;
+}
+
